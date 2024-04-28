c29398c81e76696db7c3f1326d0b579f3905ca69
Mayank Kumar <38989166+iamvkundra@users.noreply.github.com>
2024-04-28 19:41:46 +0530

    Update Main.java

--- VendingMachine/src/main/java/org/example/Main.java
+++ VendingMachine/src/main/java/org/example/Main.java
@@ -74,32 +74,4 @@ public class Main {
         ItemShelf itemShelf2 = new ItemShelf(103, new Item(ItemType.SODA, 20), 20,false);
         integerItemShelfMap.put(itemShelf2.getCode(), itemShelf2);
     }
-
-    public static int countPerms(int n) {
-        long[][] permuation = new long[n+1][5];
-        int MOD = (int) (1e9+7);
-        for(int i=0; i<5; i++) {
-            permuation[1][i] = 1;
-        }
-
-        int[][] relation = new int[][] {
-                {1}, {0,2}, {0,1,3,4}, {2,4}, {0}
-        };
-
-        for(int i=1; i<n; i++) {
-            for(int u=0; u<5;u++) {
-                permuation[i+1][u] = 0;
-                for(int v : relation[u]) {
-                    permuation[i+1][u] += permuation[i][v]%MOD;
-                }
-            }
-        }
-
-        long ans =0;
-        for(int i=0; i<5; i++) {
-            ans = (ans+permuation[n][i])%MOD;
-        }
-
-        return (int) ans;
-    }
 }
